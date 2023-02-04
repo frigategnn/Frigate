7abd2f9bae311e1c76b1d52a6e90afa42394e636
frigateresearcher <frigateresearcher@gmail.com>
2023-02-05 00:20:37 +0530

    Lipschitz generator updated

--- data/lipschitz_generator.py
+++ data/lipschitz_generator.py
@@ -1,86 +1,71 @@
-import os
-import sys
-import random
 import pickle
 import argparse
 import numpy as np
-import multiprocessing as mp
-
 import networkx as nx
-np.set_printoptions(formatter={'float':'{: >+5.2f}'.format})
-import logging
+from pathlib import Path
+import multiprocessing as mp
+from scipy.sparse import coo_array
 
-logger = logging.getLogger('lipschitz')
-logger.setLevel(logging.DEBUG)
-ch = logging.StreamHandler(sys.stdout)
-ch.setLevel(logging.DEBUG)
-fm = logging.Formatter("%(process)s - %(asctime)s - %(name)s - %(levelname)s - %(message)s")
-ch.setFormatter(fm)
-ch.flush = sys.stdout.flush
-logger.addHandler(ch)
 
-def sssplr(graph, node_range, cutoff):
-    dists_dict = {}
-    for node in node_range:
-        dists_dict[node] = nx.single_source_dijkstra_path_length(graph, node, cutoff=cutoff, weight='haversine')
-    return dists_dict
-def merge_dicts(dicts):
+def sssplr(G, nodes):
     result = {}
-    for dictionary in dicts:
-        result.update(dictionary)
+    for n in nodes:
+        result[n] = nx.single_source_dijkstra_path_length(G, n, cutoff=None, weight='haversine')
     return result
-def lipschitz_node_embeddings(nodes_forward, G, k):
-    nodes = list(nodes_forward.keys())
+
+
+def lipschitz_node_embeddings(G, nodes, k):
     G_temp = G.reverse(copy=True)
-    anchor_nodes = random.sample(nodes, k)
-    logger.info("Starting dijkstra")
-    num_workers = 32
-    cutoff = None
-    pool = mp.Pool(processes = num_workers)
-    results = [pool.apply_async(sssplr,\
-            args=(G_temp, anchor_nodes[int(k/num_workers*i):int(k/num_workers*(i+1))], cutoff))\
-            for i in range(num_workers)]
-    outputs = [p.get() for p in results]
-    dists_dict = merge_dicts(outputs)
+    anchor_nodes = np.random.choice(nodes, size=k, replace=False)
+    num_workers = 16 if k > 16 else k
+    results = []
+    per_worker = k/num_workers
+    pool = mp.Pool(processes=num_workers)
+    for n in range(num_workers):
+        start, end = int(per_worker*n), int(per_worker*(n+1))
+        results.append(
+                pool.apply_async(
+                    sssplr, args=[
+                        G_temp, anchor_nodes[start:end]]))
+    lips_dist_list = [result.get() for result in results]
     pool.close()
     pool.join()
-    logger.info('Dijkstra done')
-    embeddings = np.zeros((len(nodes),k))
+    lips_dist = {}
+    for d in lips_dist_list:
+        lips_dist.update(d)
+    embeddings = np.zeros((len(nodes), k))
     for i, node_i in enumerate(anchor_nodes):
-        shortest_dist = dists_dict[node_i]
+        sd = lips_dist[node_i]
         for j, node_j in enumerate(nodes):
-            dist = shortest_dist.get(node_j, -1)
-            if dist != -1:
-                embeddings[nodes_forward[node_j], i] = 1/(dist+1)
+            dist = sd.get(node_j, -1)
+            if dist!=-1:
+                embeddings[node_j, i] = 1/(dist+1)
     embeddings = (embeddings - embeddings.mean(axis=0))/embeddings.std(axis=0)
-    return embeddings, nodes_forward
+    return embeddings
 
-parser = argparse.ArgumentParser()
-parser.add_argument("-d","--data-home",default="chengdu_data")
-parser.add_argument("-g","--graph",default="map/graph_with_haversine.pkl")
-parser.add_argument("-o","--output-dir",default="outputs")
-args = parser.parse_args()
-logger.debug(f"OPTIONS: {vars(args)}")
-# load graph
-with open(os.path.join(os.path.dirname(os.path.abspath('.')),args.data_home,args.graph),'rb') as f:
-    g = pickle.load(f)
 
-nodes_used = set()
-for i, e in enumerate(g.edges(data=True)):
-    nodes_used.add(e[0])
-    nodes_used.add(e[1])
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--dataset", required=True, type=str,
+                        help="Path to the directory having adj_mx.pkl file, eg: Chengdu")
+    parser.add_argument('-k', type=int, default=16,
+                        help="Number of Lipschitz anchor nodes")
+    parser.add_argument('--output_filename', required=True, type=str,
+                        help="Output file name")
+    pargs = parser.parse_args()
+    # ---------------------------------------------------
+    adj_path = Path(pargs.dataset,"adj_mx.pkl")
+    with open(adj_path, "rb") as f:
+        adj_data = pickle.load(f)
+    adj = coo_array((adj_data['v'],adj_data['ij']),shape=adj_data['shape']).todense()
+    G = nx.DiGraph()
+    G.add_nodes_from(range(adj_data['shape'][0]))
+    for i,j in zip(adj_data['ij'][0],adj_data['ij'][1]):
+        G.add_edge(i, j, haversine=adj[i,j])
+    nodes = list(range(adj_data['shape'][0]))
+    embeddings = lipschitz_node_embeddings(G, nodes, pargs.k)
+    np.savez_compressed(pargs.output_filename+'.npz', lipschitz=embeddings)
 
-nodes_used = list(nodes_used)
-logger.debug(f"Number of nodes {len(nodes_used)}")
 
-nodes_forward = {node:i for i,node in enumerate(nodes_used)}
-k = 16
-logger.info(f"k={k} anchor nodes used for lipschitz embeddings.")
-em,nf=lipschitz_node_embeddings(nodes_forward, g, k)
-logger.info("Embeddings are\n{}\n{}".format(em,em.shape))
-with open(os.path.join(os.path.dirname(os.path.abspath('.')),args.output_dir,"{df}_lipschitz_data.npz".format(df=args.data_home)),"wb") as f:
-    save_dict = {'embeddings':em,'node_mappings_used':nf}
-    np.savez_compressed(f,**save_dict)
-
-logger.info("Saved output at: {}".format(os.path.join(os.path.dirname(os.path.abspath('.')),args.output_dir,"{df}_lipschitz_data.npz".format(df=args.data_home))))
-logger.info("Look for keys \"embeddings\" and \"node_mappings_used\"")
+if __name__=="__main__":
+    main()
